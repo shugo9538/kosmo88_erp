19f17232073a4446f91e4d7215e17d644f25ffb3
programmerSim <sghkkk5777@hanmail.net>
2021-09-23 18:00:02 +0900

    jhsim

--- src/main/java/com/kosmo88/logistics_erp/hr/dao/HRDAOImpl.java
+++ src/main/java/com/kosmo88/logistics_erp/hr/dao/HRDAOImpl.java
@@ -8,5 +8,107 @@ public class HRDAOImpl implements HRDAO {
     @Autowired
     SqlSession sqlSession;
 
+	@Override
+	public int insertAttendanceCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int employeeHasHoliday() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int standardCommute() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int manageSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int salayStepTable() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertAttendance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int selectAttendacne() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int attendanceStatus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int registerDocument() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int leftHolidayNum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int useHolidayNum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int attendanceTable() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int monthlyPaymentAndStatus() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int salaryStatistics() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int earnedIncome() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int paySlip() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int makePaySlipAndTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
     
 }
